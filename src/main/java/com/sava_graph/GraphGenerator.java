7bcf62755fc8af797b292da3dbf68b21f871f2e8
SantaMinus <santaminus@gmail.com>
2019-07-20 22:47:49 +0300

    Create package structure

---
+++ src/main/java/com/sava_graph/GraphGenerator.java
@@ -0,0 +1,48 @@
+package com.sava_graph;
+
+import java.io.FileNotFoundException;
+
+public class GraphGenerator {
+	
+	public String generate(int numberOfVertex, int minVertexWeight, int maxVertexWeight, double correlation, int minConnection, int maxConnection){
+		String res = "";
+		double cor = 0;
+		
+		do {
+			res = "";
+			int totalVertexWeight = 0;
+			int totalLinksWeight = 0;
+			//generate weight for each vertex
+			for(int i = 0; i < numberOfVertex; i++) {
+				int weight = (int)(Math.random() * (maxVertexWeight - minVertexWeight) + minVertexWeight);
+				totalVertexWeight += weight;
+				res = res + (i + 1) + " " + weight + "\n";
+			}
+			//generate links
+			res += "link\n";
+			for(int i = 0; i < numberOfVertex; i++) {
+				int firstNode = (int)(Math.random() * (numberOfVertex - 1) + 1);
+				int secondNode = (int)(Math.random() * (numberOfVertex - firstNode) + firstNode) + 1;
+				int weight = (int)(Math.random() * (maxConnection - minConnection) + minConnection);
+				totalLinksWeight += weight;
+				res += firstNode + " " + secondNode + " " + weight + "\n";
+			}
+			cor = Math.floor(((double)totalVertexWeight / (totalVertexWeight + totalLinksWeight)) * 100) / 100;
+			//System.out.println(cor);
+		} while(cor != correlation);
+		return res;
+	}
+	
+	
+	public static void run() {
+		GraphGenerator g = new GraphGenerator();
+		String s = g.generate(5, 1, 3, 0.5, 1, 3);
+		java.io.PrintStream ps = null;
+		try {
+			ps = new java.io.PrintStream("/home/katerynasavina/Documents/java/5.2/pzks1_gen.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		ps.print(s);
+	}
+}
---
+++ src/main/java/com/sava_graph/Solution.java
@@ -0,0 +1,91 @@
+package com.sava_graph;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
+public class Solution {
+    
+    private int n; 			//количество вершин в орграфе
+    private int color[]; 			 	//массив для хранения цветов вершин
+    private int prev[];				    //массив предков, необходимых для восстановления цикла, в случае его наличия в графе
+    private boolean cyclic = false; 	//флаг, показывающий содержит орграф цикл или нет
+    ArrayList<Integer> cycle; 			//цикл
+    
+    //процедура обхода в глубину
+    private void dfsCycle(int v) { 
+        //if node is black
+        if (color[v] == 2) { 
+            return;
+        }
+        if (cyclic) { 
+            return;
+        }
+        //if node is grey it means graph is cyclic
+        if (color[v] == 1) { 
+            cyclic = true;
+            cycle.add(v);		//save cycle 
+            for (int ver = prev[v]; ver != v; ver = prev[ver]) {
+                cycle.add(ver);
+            }
+            cycle.add(v);
+            for (int i = 0; i < cycle.size() / 2; ++i) {
+                int temp = cycle.get(i);
+                cycle.set(i, cycle.get(cycle.size() - 1 - i));
+                cycle.set(cycle.size() - 1 - i, temp);
+            }
+            return;
+        }
+        color[v] = 1;		//mark node grey
+        //запускаем обход из всех вершин, смежных с вершиной v
+        int w;
+        for (int i = 0; i < Maths.matrix.length; ++i) { 
+            if(Maths.matrix[v][i] != 0) {
+            	//w = Maths.matrix[v][i];
+            	w = i;
+            	prev[w] = v; 			//вершина v - это предок вершины w при обходе
+            	dfsCycle(w); 			//вызов обхода от вершины w, смежной с вершиной v
+            }
+            if (cyclic) {
+                return;
+            }
+        }
+        color[v] = 2; 		//mark black
+    }
+    
+    private void readData() throws IOException {
+    	n = Maths.matrix.length;	//number of nodes
+    	//mark nodes as white
+        color = new int[n];
+        Arrays.fill(color, 0);
+        //ancestors array
+        prev = new int[n];
+        Arrays.fill(prev, -1);
+        
+        cyclic = false;
+        cycle = new ArrayList<Integer>();
+    }
+   
+    private void printData() throws IOException {
+        if (cyclic) {
+        	String s = "";
+            for (int i = 0; i < cycle.size(); ++i) {
+            	s += (cycle.get(i) + 1) + " ";
+            }
+        	JOptionPane.showMessageDialog(UI.frame, "Graph has cycle: " + s, "ERROR", JOptionPane.ERROR_MESSAGE);
+        	System.exit(0);
+        } else {
+        	JOptionPane.showMessageDialog(UI.frame, "Graph is acyclic");
+        }
+    }
+    
+    void run() throws IOException {
+        readData();
+        for (int v = 0; v < n; ++v) {
+            dfsCycle(v);
+        }
+        printData();
+    }
+}
